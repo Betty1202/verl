28441bad230a7ff91b905ef02353eb01282aa606
hxyhxy <hxyhxy@stu.xjtu.edu.cn>
2025-08-13 14:57:20 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 pip install torchdata
 python -c "from datasets import load_from_disk; \
+import os; \
 data_paths='data/sweperf_training_0812'; \
 dataset = load_from_disk(data_paths); \
-parquet_path = os.path.join(data_paths,"data.parquet"); \
+parquet_path = os.path.join(data_paths,'data.parquet'); \
 dataset.to_parquet(parquet_path)"
 python -c "from datasets import load_from_disk; \
+import os; \
 data_paths='data/sweperf_testing_0812'; \
 dataset = load_from_disk(data_paths); \
-parquet_path = os.path.join(data_paths,"data.parquet"); \
+parquet_path = os.path.join(data_paths,'data.parquet'); \
 dataset.to_parquet(parquet_path)"
